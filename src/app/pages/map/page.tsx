ad62d119b931c06efafae3b1cc4dcc1dfaa354ca
Zyprush <alberiojake27@gmail.com>
2024-08-19 17:34:16 +0800

    psuh

--- src/app/pages/map/page.tsx
+++ src/app/pages/map/page.tsx
@@ -7,8 +7,8 @@ const Map = () => {
         <Layout>
             {/* <Authenticator /> */}
             <div className="flex flex-1 h-screen">
-                <div className="p-2 border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
-                    <PaluanMapData/>
+                <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+                    <PaluanMapData />
                 </div>
             </div>
         </Layout>
