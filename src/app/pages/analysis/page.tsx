a41e0131f21965fb9d537d634f3659265ad5c6dd
Zyprush <alberiojake27@gmail.com>
2024-08-25 15:05:33 +0800

    push

--- src/app/pages/analysis/page.tsx
+++ src/app/pages/analysis/page.tsx
@@ -6,7 +6,7 @@ const Analysis = () => {
         <Layout>
             <Authenticator/>
             <div className="flex flex-1 h-screen">
-                <div className="p-2 md:p-8 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+                <div className="p-2 md:p-2 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
                     <div className="flex gap-2">
                         {[...new Array(4)].map((i) => (
                             <div
