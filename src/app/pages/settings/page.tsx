34475f804f64b616b8cdf80d7f61e4fb80630d3c
zanzen <olsencortuna@gmail.com>
2024-12-22 12:11:44 +0800

    Enhance settings page: Added a download link for the user guide with an icon, improving user accessibility to documentation.

--- src/app/pages/settings/page.tsx
+++ src/app/pages/settings/page.tsx
@@ -2,7 +2,7 @@
 import { Authenticator } from "@/components/Authenthicator";
 import { Layout } from "@/components/Layout";
 import SettingsItem from "@/components/SettingsItem";
-import { IconPassword, IconSettings } from "@tabler/icons-react";
+import { IconDownload, IconPassword, IconSettings } from "@tabler/icons-react";
 import Link from "next/link";
 import Sitio from "./Sitio";
 import BarangayComponent from "./Barangayx";
@@ -27,6 +27,22 @@ const Settings = () => {
             <div className="h-full">
               <SettingsItem title="Address" name="address" row={1} />
             </div>
+
+            <a
+              href="/user-guide.docx" // Path to the file in the public folder
+              download="User-Guide.docx" // Suggested filename
+              className="p-4 flex flex-col bg-white text-zinc-600 dark:text-zinc-200 dark:bg-neutral-800 rounded-lg dark:border shadow-sm dark:border-neutral-700 dark:border-opacity-50 h-full dark:bg-opacity-40"
+            >
+              <span className="flex font-semibold justify-between mt-1">
+                <h1>User Guide</h1>
+                <span className="p-1 text-lg border text-zinc-600 dark:text-zinc-200 bg-zinc-100 bg-opacity-40 dark:bg-zinc-800 border-zinc-500 rounded-md dark:border-zinc-400 border-opacity-30 dark:border-opacity-30">
+                  <IconDownload />
+                </span>
+              </span>
+              <p className="mt-auto mb-0 text-sm text-zinc-500 dark:text-zinc-300">
+                Download user guide
+              </p>
+            </a>
 
             {/* PIN Card */}
             <Link
@@ -66,7 +82,6 @@ const Settings = () => {
             <BarangayComponent />
             <SettingsItem title="About Us" name="aboutUs" row={5} />
           </div>
-
         </div>
       </div>
     </Layout>
